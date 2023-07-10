7efb98281643d2069d05164b4eb830e426ddafe7
chris <fernandes.christ14@gmail.com>
2023-07-09 23:34:05 -0300

    [Commit] Retirada de imports desnecessários

--- app/src/main/java/com/example/jokempo/GameFragment.kt
+++ app/src/main/java/com/example/jokempo/GameFragment.kt
@@ -6,8 +6,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.LifecycleOwner
-import androidx.lifecycle.Observer
 import com.example.jokempo.databinding.FragmentGameBinding
 
 class GameFragment : Fragment() {
